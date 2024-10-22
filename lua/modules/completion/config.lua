4ac1fea8cead9437c147f668ee0d271a1ebc3304
DarkKronicle <darkkronicle@gmail.com>
2024-10-22 12:48:29 -0600

    feat: disable codeium by default

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -43,6 +43,43 @@ end
 
 function M.cmp()
     local codeium = require('codeium')
+
+    -- https://github.com/Exafunction/codeium.nvim/issues/136
+    local Source = require("codeium.source")
+
+    vim.g["codeium_enabled"] = false
+
+    local function is_codeium_enabled()
+        local enabled = vim.b["codeium_enabled"]
+        if enabled == nil then
+            enabled = vim.g["codeium_enabled"]
+            if enabled == nil then
+                enabled = true -- enable by default
+            end
+        end
+        return enabled
+    end
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    function Source:is_available()
+        local enabled = is_codeium_enabled()
+        ---@diagnostic disable-next-line: undefined-field
+        return enabled and self.server.is_healthy()
+    end
+
+    vim.api.nvim_set_keymap('n', '<leader>tC', '', {
+        callback = function()
+            local new_enabled = not is_codeium_enabled()
+            vim.b["codeium_enabled"] = new_enabled
+            if new_enabled then
+                vim.notify("Codeium enabled in buffer")
+            else
+                vim.notify("Codeium disabled in buffer")
+            end
+        end,
+        noremap = true
+    })
+
     codeium.setup()
     local cmp = require('cmp')
     cmp.setup({
